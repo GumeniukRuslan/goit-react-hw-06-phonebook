9c475c0489cb4aedaaa88ad9e4d2d619e09197ab
GumeniukRuslan <bishob1997@gmail.com>
2023-06-25 12:49:30 +0300

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
+import { useEffect, useState } from "react";
+import { ContactForm } from "./ContactFrom/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+import { Filter } from "./Filter/Filter";
+import { toast, Toaster } from "react-hot-toast";
+
 export const App = () => {
+
+  const [contacts, setContacts] = useState(localStorage.getItem('contacts') ? JSON.parse(localStorage.getItem('contacts')) : []);
+  const [filter, setFilter] = useState();
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts])
+
+  const saveContact = (contact) => {
+    if (contacts.find(obj => obj.name.toLowerCase() === contact.name.toLowerCase())) {
+      const notify = () => toast.error('Contact is already in list.');
+      return notify()
+    }
+    setContacts((prevContacts) => [...prevContacts, contact])
+  };
+
+  const deleteContact = (evt) => setContacts(contacts.filter(contact => contact.id !== evt.target.closest('li').id));
+
+  const filterContacts = (evt) => setFilter(evt.target.value.trim());
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm saveContact={saveContact} />
+
+      {contacts.length
+        ? <><h2>Contacts</h2>
+            <Filter filterValue={filterContacts} />
+            <ContactList deleteContact={deleteContact} contacts={filter
+              ? contacts.filter(obj => obj.name.toLowerCase().includes(filter.toLowerCase()))
+              : contacts} />
+          </>
+        : <p>Add some contacts</p>}
+      <Toaster position="top-right" toastOptions={{duration: 1500}} />
     </div>
   );
 };
