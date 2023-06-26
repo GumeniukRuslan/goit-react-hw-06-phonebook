39fddc896f0ea15974bd6133942baadabd917daa
GumeniukRuslan <bishob1997@gmail.com>
2023-06-26 11:34:05 +0300

    Update ContactForm.jsx

--- src/components/ContactFrom/ContactForm.jsx
+++ src/components/ContactFrom/ContactForm.jsx
@@ -31,13 +31,13 @@ export const ContactForm = ({saveContact}) => {
     <form onSubmit={submitForm}>
       <div>
         <label htmlFor="userName">Name</label>
-        <input onChange={handleChange} id="userName" type="text" name="name" pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        <input onChange={handleChange} id="userName" type="text" name="name" pattern="^[A-Za-z\u0080-\uFFFF ']+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required />
       </div>
       <div>
         <label htmlFor="userNumber">Number</label>
-        <input onChange={handleChange} id="userNumber" type="tel" name="number" pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        <input onChange={handleChange} id="userNumber" type="tel" name="number" pattern= "^(\+?[0-9.\(\)\-\s]*)$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required />
       </div>
